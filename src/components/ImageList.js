2d6f3248aace74d7d5b246104c0d7fbe4d644b4b
Andre Costa <andrecondecosta@gmail.com>
2024-05-13 21:42:34 +0100

    create delete botton in imagelist

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -20,16 +20,41 @@ function ImageList({ category = "" }) {
       console.error(`Error fetching ${category} images:`, error);
     }
   };
+
+  const deleteImage = async (id) => {
+    const response = await fetch(`http://127.0.0.1:3000/api/v1/${category}/${id}`, {
+      method: 'DELETE',
+    });
+    if (response.ok) {
+      setImages(images.filter((image) => image.id !== id));
+    }
+  };
+
   return (
     <div>
-      <h1> Images</h1>
+      <h1> {category}</h1>
       {images.map((image, index) => (
-        <img
-          key={index}
-          src={image.image_data}
-          alt={image.title}
-          style={{ width: '200px', height: '130px', margin: "25px" }}
-        />
+        <div key={index} style={{ position: 'relative', display: 'inline-block' }}>
+          <img
+            src={image.image_data}
+            alt={image.title}
+            style={{ width: '200px', height: '130px', margin: "25px" }} // image_data já é uma URL
+          />
+          <button
+            style={{
+              position: 'absolute',
+              top: 0,
+              right: 0,
+              background: 'red',
+              color: 'white',
+              border: 'none',
+              cursor: 'pointer'
+            }}
+            onClick={() => deleteImage(image.id)}
+          >
+            X
+          </button>
+        </div>
       ))}
     </div>
   );
