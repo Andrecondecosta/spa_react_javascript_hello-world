a69162863ca03390e461ac301f2d88f2849557d0
Andre Costa <andrecondecosta@gmail.com>
2024-06-07 20:14:32 +0100

    Fix auth0-provider 5

--- src/components/buttons/signup-button.js
+++ src/components/buttons/signup-button.js
@@ -7,7 +7,7 @@ export const SignupButton = () => {
  const handleSignUp = async () => {
    await loginWithRedirect({
      appState: {
-       returnTo: "/",
+       returnTo: "/admin",
      },
      authorizationParams: {
        screen_hint: "signup",
