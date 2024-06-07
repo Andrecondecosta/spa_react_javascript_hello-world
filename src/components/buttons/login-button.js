dc60ecc2037d428f1604cad3db064f8ff2045f8c
Andre Costa <andrecondecosta@gmail.com>
2024-06-07 20:30:10 +0100

    Fix auth0-provider 6

--- src/components/buttons/login-button.js
+++ src/components/buttons/login-button.js
@@ -2,19 +2,19 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 export const LoginButton = () => {
- const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
- const handleLogin = async () => {
-   await loginWithRedirect({
-     appState: {
-       returnTo: "/admin",
-     },
-   });
- };
+  const handleLogin = async () => {
+    await loginWithRedirect({
+      appState: {
+        returnTo: "/admin",
+      },
+    });
+  };
 
- return (
-   <button className="button__login" onClick={handleLogin}>
-     Log In
-   </button>
- );
+  return (
+    <button className="button__login" onClick={handleLogin}>
+      Log In
+    </button>
+  );
 };
