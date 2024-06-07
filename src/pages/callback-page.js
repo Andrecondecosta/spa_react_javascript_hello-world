e51ead9ad670842d56321ca4235bd4569da5c2ce
Andre Costa <andrecondecosta@gmail.com>
2024-06-07 19:23:11 +0100

    Fix auth0-provider

--- src/pages/callback-page.js
+++ src/pages/callback-page.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 import { NavBar } from "../components/navigation/desktop/nav-bar";
 import { MobileNavBar } from "../components/navigation/mobile/mobile-nav-bar";
 
 export const CallbackPage = () => {
- return (
-   <div className="page-layout">
-     <NavBar />
-     <MobileNavBar />
-     <div className="page-layout__content" />
-   </div>
- );
+  const { handleRedirectCallback } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const processCallback = async () => {
+      try {
+        await handleRedirectCallback();
+        navigate('/'); // Redireciona para a página inicial após a autenticação
+      } catch (error) {
+        console.error('Erro de autenticação:', error);
+      }
+    };
+
+    processCallback();
+  }, [handleRedirectCallback, navigate]);
+
+  return (
+    <div className="page-layout">
+      <NavBar />
+      <MobileNavBar />
+      <div className="page-layout__content">
+        <p>Processing login...</p>
+      </div>
+    </div>
+  );
 };
