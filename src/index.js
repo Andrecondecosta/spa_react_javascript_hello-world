7a874b131e44602123bd388a95797c6ab416da86
Andre Costa <andrecondecosta@gmail.com>
2024-07-22 22:54:21 +0100

    route callback 3

--- src/index.js
+++ src/index.js
@@ -5,8 +5,14 @@ import { App } from "./app";
 import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
 import "./styles/styles.css";
 
+console.log('Auth0 Domain:', process.env.REACT_APP_AUTH0_DOMAIN);
+console.log('Auth0 Client ID:', process.env.REACT_APP_AUTH0_CLIENT_ID);
+console.log('Auth0 Callback URL:', process.env.REACT_APP_AUTH0_CALLBACK_URL);
+console.log('API Server URL:', process.env.REACT_APP_API_SERVER_URL);
+
 const container = document.getElementById("root");
 const root = createRoot(container);
+
 
 root.render(
  <React.StrictMode>
