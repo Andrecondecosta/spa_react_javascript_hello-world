0d2b67ba7aa7e2856a2164f0ac375b9fd32d45b1
Andre Costa <andrecondecosta@gmail.com>
2024-07-24 17:58:25 +0100

    change Auth0ProviderWithNavigate page2

--- src/auth0-provider-with-navigate.js
+++ src/auth0-provider-with-navigate.js
@@ -1,32 +1,31 @@
-import { Auth0Provider } from "@auth0/auth0-react";
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
 
-export const Auth0ProviderWithNavigate = ({ children }) => {
+export const CallbackPage = () => {
+  const { handleRedirectCallback } = useAuth0();
   const navigate = useNavigate();
 
-  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
+  useEffect(() => {
+    const processCallback = async () => {
+      try {
+        console.log('Processing callback...');
+        await handleRedirectCallback();
+        console.log('Callback processed, navigating to home...');
+        navigate('/'); // Redireciona para a página inicial ou outra página desejada
+      } catch (error) {
+        console.error('Erro de autenticação:', error);
+      }
+    };
 
-  const onRedirectCallback = (appState) => {
-    console.log('onRedirectCallback called', appState);
-    navigate(appState?.returnTo || window.location.pathname); // Redireciona para a página inicial ou outra página desejada
-  };
-
-  if (!(domain && clientId && redirectUri)) {
-    console.error("Auth0 configuration is missing");
-    return null;
-  }
+    processCallback();
+  }, [handleRedirectCallback, navigate]);
 
   return (
-    <Auth0Provider
-      domain={domain}
-      clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
-      onRedirectCallback={onRedirectCallback}
-    >
-      {children}
-    </Auth0Provider>
+    <div className="page-layout">
+      <div className="page-layout__content">
+        <p>Processing login...</p>
+      </div>
+    </div>
   );
 };
