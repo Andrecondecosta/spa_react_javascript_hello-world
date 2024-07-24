95230c8574fe2df3f4a620b06f1ec301f8764b88
Andre Costa <andrecondecosta@gmail.com>
2024-07-24 18:02:02 +0100

    change Auth0ProviderWithNavigate page3

--- src/auth0-provider-with-navigate.js
+++ src/auth0-provider-with-navigate.js
@@ -1,31 +1,31 @@
-import React, { useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export const CallbackPage = () => {
-  const { handleRedirectCallback } = useAuth0();
+export const Auth0ProviderWithNavigate = ({ children }) => {
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const processCallback = async () => {
-      try {
-        console.log('Processing callback...');
-        await handleRedirectCallback();
-        console.log('Callback processed, navigating to home...');
-        navigate('/'); // Redireciona para a página inicial ou outra página desejada
-      } catch (error) {
-        console.error('Erro de autenticação:', error);
-      }
-    };
+  const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+  const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+  const redirectUri = process.env.REACT_APP_AUTH0_CALLBACK_URL;
 
-    processCallback();
-  }, [handleRedirectCallback, navigate]);
+  const onRedirectCallback = (appState) => {
+    navigate(appState?.returnTo || '/'); // Redireciona para a página inicial ou outra página desejada
+  };
+
+  if (!(domain && clientId && redirectUri)) {
+    console.error("Auth0 configuration is missing");
+    return null;
+  }
 
   return (
-    <div className="page-layout">
-      <div className="page-layout__content">
-        <p>Processing login...</p>
-      </div>
-    </div>
+    <Auth0Provider
+      domain={domain}
+      clientId={clientId}
+      authorizationParams={{ redirect_uri: redirectUri }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
   );
 };
